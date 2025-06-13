21f389beee98d8a714d3e8a39837b2fa936d87cb
Alejandro Lopez <alejandro@unity3d.com>
2025-06-13 11:26:23 -0500

    Initial commit with example

---
+++ Assets/Netcode Test/Scripts/PlayerAuthoring.cs
@@ -0,0 +1,36 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.NetCode;
+using UnityEngine;
+
+public struct Player : IComponentData
+{
+    public Entity Character;
+    public Entity Controller;
+
+    public Entity Hand;
+    
+}
+
+[DisallowMultipleComponent]
+public class PlayerAuthoring : MonoBehaviour
+{
+    [SerializeField] private GameObject _character;
+    [SerializeField] private GameObject _controller;
+
+    [SerializeField] private GameObject _hand;
+    class Baker : Baker<PlayerAuthoring>
+    {
+        public override void Bake(PlayerAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent<Player>(entity, new()
+            {
+                Character = GetEntity(authoring._character, TransformUsageFlags.Dynamic),
+                Controller = GetEntity(authoring._controller, TransformUsageFlags.Dynamic),
+                Hand = GetEntity(authoring._hand, TransformUsageFlags.Dynamic),
+
+            });
+        }
+    }
+}
